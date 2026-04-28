6c9cc7d3d16eea8e4678d0a5e0657fe2ecb87b1e
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2026-04-28 23:27:28 +0100

    feat: add 'docs' command to open BSH Engine CLI documentation in browser

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,12 @@
 import type { CommandDefinition } from '@src/lib/cli/definition';
 import init from '@commands/init';
 import plugins from '@commands/plugins';
+import docs from '@commands/docs';
 
 const commands: CommandDefinition[] = [
   init,
   plugins,
+  docs,
 ];
 
 export default commands;
